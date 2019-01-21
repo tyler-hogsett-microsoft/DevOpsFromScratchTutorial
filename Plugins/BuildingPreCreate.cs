1cc51ba1771fce39941dc3b3b185978b7a44ba30
Tyler Hogsett <tyhogset@microsoft.com>
2019-01-20 17:13:58 -0800

    Added plugins

---
+++ Plugins/BuildingPreCreate.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugins
+{
+    [CrmPluginRegistration(
+        MessageNameEnum.Create,
+        "cou_building",
+        StageEnum.PreOperation,
+        ExecutionModeEnum.Synchronous,
+        "",
+        "cou_building_precreate",
+        1,
+        IsolationModeEnum.Sandbox)]
+    public class BuildingPreCreate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            tracer.Trace("Pre-create plugin");
+        }
+    }
+}
